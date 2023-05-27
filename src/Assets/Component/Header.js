54256150da086f734a99dbafeb7c92954b919304
Bit0 <hosybaonhan2004@gmail.com>
2023-05-27 09:19:38 -0700

    responsive header

--- src/Assets/Component/Header.js
+++ src/Assets/Component/Header.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import logo from '../Img/logo.svg';
+import bar from '../Img/bars-solid.svg'
 import '../CSS/Header.css';
 
 function Header() {
@@ -13,12 +14,27 @@ function Header() {
             setHeaderClass("Header");
         }
     }
-
     window.onscroll=handleScroll;
+
+    var [barClass,setBarClass]=useState('invisible');
+    let handleBar=()=> {
+        if(barClass==='invisible') {
+            setBarClass('mobile-nav');
+        }
+        else setBarClass('invisible');
+    }
+
   return (
     <div className={HeaderClass}>
         <img src={logo} alt='logo'/>
         <ul className="navbar">
+            <li key='1'><a href="/">Trang chủ</a></li>
+            <li key='2'><a href="/News">Thông tin</a></li>
+            <li key='3'><a href="/">Phòng chống</a></li>
+            <li key='4'><a href="/about-us">Về chúng tôi</a></li>
+        </ul>
+        <img src={bar} alt='bar' className='bar-icon' onClick={handleBar}/>
+        <ul className={barClass}>
             <li key='1'><a href="/">Trang chủ</a></li>
             <li key='2'><a href="/News">Thông tin</a></li>
             <li key='3'><a href="/">Phòng chống</a></li>
