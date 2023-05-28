07e736ddc7e41227fdc2d0c008e72694ce8a6733
Bit0 <hosybaonhan2004@gmail.com>
2023-05-28 08:36:38 +0700

    responsive contentpane

--- src/Assets/Components/MainPage.js
+++ src/Assets/Components/MainPage.js
@@ -8,7 +8,8 @@ function MainPage() {
         <div className='intro-img'></div>
         <div className='intro-page'>
           <div className='intro-contentpane'>
-            
+            <h1>Bảo vệ rừng</h1>
+            <p>Hãy bảo vệ rừng của chúng ta, đó là nguồn sống của tất cả chúng ta và là một phần quan trọng của hệ sinh thái trái đất.</p>
           </div>
         </div>
         <div className='news-page'>
