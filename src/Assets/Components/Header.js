8c621e5059ce7cc68ea3b9b3cc340d4de68937f4
vkutin12q <thinhnp.22git@vku.udn.vn>
2023-05-31 09:45:41 +0700

    Add logo

--- src/Assets/Components/Header.js
+++ src/Assets/Components/Header.js
@@ -1,66 +1,105 @@
 import { useEffect, useState } from "react";
-import logo from '../Img/logo.svg';
-import bar from '../Img/bars-solid.svg'
-import '../CSS/Header.css';
+import logo from "../Img/logo.png";
+import bar from "../Img/bars-solid.svg";
+import "../CSS/Header.css";
 import { Link, useLocation } from "react-router-dom";
 
 function Header() {
-    const [mobileNavStyle,setMobileNavStyle]=useState({top:'100px'});
-    const [HeaderClass,setHeaderClass]=useState("Header");
-    let handleScroll=()=> {
-        if(window.pageYOffset>1) {
-            setHeaderClass("Header white-header");
-            setMobileNavStyle({top:'70px'});
-        }
-        else {
-            setHeaderClass("Header");
-            setMobileNavStyle({top:'100px'});
-        }
-        
+  const [mobileNavStyle, setMobileNavStyle] = useState({ top: "100px" });
+  const [HeaderClass, setHeaderClass] = useState("Header");
+  let handleScroll = () => {
+    if (window.pageYOffset > 1) {
+      setHeaderClass("Header white-header");
+      setMobileNavStyle({ top: "70px" });
+    } else {
+      setHeaderClass("Header");
+      setMobileNavStyle({ top: "100px" });
     }
-    window.onscroll=handleScroll;
+  };
+  window.onscroll = handleScroll;
 
-    var [barClass,setBarClass]=useState('mobile-nav');
-    let handleBar=()=> {
-        if(barClass==='mobile-nav') {
-            setBarClass('mobile-nav active-nav');
-        }
-        else setBarClass('mobile-nav');
+  var [barClass, setBarClass] = useState("mobile-nav");
+  let handleBar = () => {
+    if (barClass === "mobile-nav") {
+      setBarClass("mobile-nav active-nav");
+    } else setBarClass("mobile-nav");
+  };
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth > 600) {
+      setBarClass("mobile-nav");
     }
+  });
+  const CurrLink = useLocation();
 
-    window.addEventListener("resize", ()=>{
-        if(window.innerWidth>600) {
-            setBarClass('mobile-nav');
-        }
-    });
-    const CurrLink=useLocation();
-
-    const [link1,setLink1]=useState("/"===window.location.pathname?"curr-page":"");
-    const [link2,setLink2]=useState(window.location.pathname.includes("/News")?"curr-page":"");
-    const [link3,setLink3]=useState(window.location.pathname.includes("/Report")?"curr-page":"");
-    const [link4,setLink4]=useState(window.location.pathname.includes("/about-us")?"curr-page":"");
-    useEffect(()=>{
-        setLink1("/"===window.location.pathname?"curr-page":"");
-        setLink2(window.location.pathname.includes("/News")?"curr-page":"");
-        setLink3(window.location.pathname.includes("/Report")?"curr-page":"");
-        setLink4(window.location.pathname.includes("/about-us")?"curr-page":"");
-    },[CurrLink]) 
+  const [link1, setLink1] = useState(
+    "/" === window.location.pathname ? "curr-page" : ""
+  );
+  const [link2, setLink2] = useState(
+    window.location.pathname.includes("/News") ? "curr-page" : ""
+  );
+  const [link3, setLink3] = useState(
+    window.location.pathname.includes("/Report") ? "curr-page" : ""
+  );
+  const [link4, setLink4] = useState(
+    window.location.pathname.includes("/about-us") ? "curr-page" : ""
+  );
+  useEffect(() => {
+    setLink1("/" === window.location.pathname ? "curr-page" : "");
+    setLink2(window.location.pathname.includes("/News") ? "curr-page" : "");
+    setLink3(window.location.pathname.includes("/Report") ? "curr-page" : "");
+    setLink4(window.location.pathname.includes("/about-us") ? "curr-page" : "");
+  }, [CurrLink]);
   return (
     <div className={HeaderClass}>
-        <Link to="/"><img src={logo} alt='logo'/></Link>
-        <ul className="navbar">
-            <li key='1'><Link to="/" className={link1}>Trang chủ</Link></li>
-            <li key='2'><Link to="/News" className={link2} >Thông tin</Link></li>
-            <li key='3'><Link to="/Report" className={link3}>Báo cáo</Link></li>
-            <li key='4'><Link to="/about-us" className={link4}>Về chúng tôi</Link></li>
-        </ul>
-        <img src={bar} alt='bar' className='bar-icon' onClick={handleBar}/>
-        <ul className={barClass} style={mobileNavStyle}>
-            <li key='1' onClick={handleBar}><Link to="/" className={link1}>Trang chủ</Link></li>
-            <li key='2' onClick={handleBar}><Link to="/News" className={link2}>Thông tin</Link></li>
-            <li key='3' onClick={handleBar}><Link to="/Report" className={link3}>Báo cáo</Link></li>
-            <li key='4' onClick={handleBar}><Link to="/about-us" className={link4}>Về chúng tôi</Link></li>
-        </ul>
+      <Link to="/">
+        <img className="logo" src={logo} alt="logo" />
+      </Link>
+      <ul className="navbar">
+        <li key="1">
+          <Link to="/" className={link1}>
+            Trang chủ
+          </Link>
+        </li>
+        <li key="2">
+          <Link to="/News" className={link2}>
+            Thông tin
+          </Link>
+        </li>
+        <li key="3">
+          <Link to="/Report" className={link3}>
+            Báo cáo
+          </Link>
+        </li>
+        <li key="4">
+          <Link to="/about-us" className={link4}>
+            Về chúng tôi
+          </Link>
+        </li>
+      </ul>
+      <img src={bar} alt="bar" className="bar-icon" onClick={handleBar} />
+      <ul className={barClass} style={mobileNavStyle}>
+        <li key="1" onClick={handleBar}>
+          <Link to="/" className={link1}>
+            Trang chủ
+          </Link>
+        </li>
+        <li key="2" onClick={handleBar}>
+          <Link to="/News" className={link2}>
+            Thông tin
+          </Link>
+        </li>
+        <li key="3" onClick={handleBar}>
+          <Link to="/Report" className={link3}>
+            Báo cáo
+          </Link>
+        </li>
+        <li key="4" onClick={handleBar}>
+          <Link to="/about-us" className={link4}>
+            Về chúng tôi
+          </Link>
+        </li>
+      </ul>
     </div>
   );
 }
