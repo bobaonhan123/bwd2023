7cdbe1c8d2ca8546e4660a2a9c710fd00c87d66e
Bit0 <hosybaonhan2004@gmail.com>
2023-06-02 19:00:05 -0700

    fixed resolution for the image on about us

--- src/Assets/Components/Pages/Report.js
+++ src/Assets/Components/Pages/Report.js
@@ -65,12 +65,12 @@ const Report = () => {
                     <p className="left-text ">
                     Các số liệu của Tổng cục Thống kê cho thấy, trong 10 năm của giai đoạn 2009-2018, nạn cháy rừng đã thiêu hủy gần 22 nghìn ha rừng của Việt Nam, gây thiệt hại lớn về kinh tế cho đất nước.
                     </p>
-                  <img className="report_img_1 " src="external/Thuc-trang-cong-tac-phong-chay-chua-chay-rung1.jpg "></img>
+                  <img className="report_img_1 " src="external/Thuc-trang-cong-tac-phong-chay-chua-chay-rung1.jpg " alt=''></img>
               </div>
             </AnimatedLeftToRight>
             <AnimatedRightToLeft>
               <div className="row">
-                  <img className="report_img_1" src="external/van-nan-chay-rung-o-viet-nam.jpg"></img>
+                  <img className="report_img_1" src="external/van-nan-chay-rung-o-viet-nam.jpg" alt=''></img>
                   <p className="right-text">
                   Đỉnh điểm của giai đoạn này là vào năm 2010, khoảng 6.723 ha rừng đã bị lửa lớn thiêu rụi do nắng hạn kéo dài; năm 2007 xảy ra 749 vụ cháy rừng gây thiệt hại 4.188 ha. Vài năm trở lại đây, diện tích rừng bị cháy tuy có giảm mạnh, nhưng vẫn tồn tại những diễn biến bất ngờ và phức tạp khó lường
                   </p>                
@@ -81,7 +81,7 @@ const Report = () => {
                   <p className="center-text ">
                   Vài năm trở lại đây, diện tích rừng bị cháy tuy có giảm mạnh, nhưng vẫn tồn tại những diễn biến bất ngờ và phức tạp khó lường. Đến năm 2018, thiệt hại do cháy rừng tuy có tăng so với năm 2017 (739,1 ha) nhưng nhìn chung, thiệt hại vẫn ở mức thấp so với các năm khác. Tuy nhiên, trong 10 tháng năm 2019, diện tích rừng bị cháy lại tăng lên đến 2,7 nghìn ha, gấp 3,6 lần năm 2018.                </p>
                   <br></br> <br></br> 
-                  <img className="report_img_1 " src="external/5-9chay.jpg "></img>
+                  <img className="report_img_1 " src="external/5-9chay.jpg " alt=''></img>
               </div>
             </AnimatedBottomToTop>
           </div>
